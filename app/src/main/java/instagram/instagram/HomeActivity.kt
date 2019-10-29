ba9181314e1c00f61785bdc8fc7921cae284c54b
Vasiliy Samargin <vasiliys492@gmail.com>
2019-10-29 16:52:10 +0300

    course-3

--- app/src/main/java/instagram/instagram/HomeActivity.kt
+++ app/src/main/java/instagram/instagram/HomeActivity.kt
@@ -4,7 +4,8 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import kotlinx.android.synthetic.main.activity_main.*
 
-class MainActivity : AppCompatActivity() {
+class HomeActivity : AppCompatActivity() {
+    private val TAG = "HomeActivity"
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -16,6 +17,16 @@ class MainActivity : AppCompatActivity() {
         for (i in 0 until bottom_navigation_view.menu.size()) {
             bottom_navigation_view.setIconTintList(i, null);
         }
+        bottom_navigation_view.setOnNavigationItemSelectedListener {
+            when(it.itemId) {
+                R.id.nav_item_home -> android.util.Log.d(TAG, "msg: nav_item_home")
+                R.id.nav_item_search -> android.util.Log.d(TAG, "msg: nav_item_search")
+                R.id.nav_item_share -> android.util.Log.d(TAG, "msg: nav_item_share")
+                R.id.nav_item_likes -> android.util.Log.d(TAG, "msg: nav_item_likes")
+                R.id.nav_item_profiles -> android.util.Log.d(TAG, "msg: nav_item_profiles")
+            }
+            false
 
+        }
     }
 }
